8497ccc805b9c5bfb950cbe6311ecba17f0d8940
puzzleweaver <puzzleweaver@gmail.com>
2016-02-09 21:57:11 -0500

    made text not suck

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 import java.awt.Font;
 import java.io.InputStream;
+import java.util.Random;
 
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
@@ -16,7 +17,9 @@ public class Main extends BasicGame {
 	
 	public static int SCALE = 2;
 	
-	public static TrueTypeFont font;
+	public static TrueTypeFont fontSmall, fontMedium, fontLarge;
+	
+	public static Random r = new Random();
 	
 	public Main(String title) {
 		super(title);
@@ -39,10 +42,13 @@ public class Main extends BasicGame {
 		Images.load();
 		World.init(gc);
 		InputStream inputStream = ResourceLoader.getResourceAsStream("res/half_bold_pixel-7.ttf");
+//		InputStream inputStream = ResourceLoader.getResourceAsStream("res/Hardpixel.OTF");
 		Font awtFont;
 		try {
 			awtFont = Font.createFont(Font.TRUETYPE_FONT, inputStream);
-			font = new TrueTypeFont(awtFont.deriveFont((float) Math.min(48, gc.getWidth()/80)), false);
+			fontSmall = new TrueTypeFont(awtFont.deriveFont((float) Math.min(48, gc.getWidth()/160)), false);
+			fontMedium = new TrueTypeFont(awtFont.deriveFont((float) Math.min(48, gc.getWidth()/80)), false);
+			fontLarge = new TrueTypeFont(awtFont.deriveFont((float) gc.getWidth()/40), true);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -50,6 +56,12 @@ public class Main extends BasicGame {
 	public void render(GameContainer gc, Graphics g) throws SlickException {
 		g.scale(SCALE, SCALE);
 		World.render(gc, g);
+		g.scale(1.0f/SCALE, 1.0f/SCALE);
+		g.setFont((org.newdawn.slick.Font) fontLarge);
+		Text.render(g);
+//		Image textImg = new Image(gc.getWidth(), gc.getHeight());
+//		Text.render(textImg.getGraphics());
+//		g.drawImage(textImg, 0, 0);
 	}
 	public void update(GameContainer gc, int dt) throws SlickException {
 		World.update(gc, dt);
--- src/main/World.java
+++ src/main/World.java
@@ -1,6 +1,6 @@
 package main;
 
-import org.newdawn.slick.Color;
+import org.newdawn.slick.Font;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
@@ -9,17 +9,17 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.tiled.TiledMap;
 
 public class World {
-	
+
 	private static TiledMap map;
-	
+
 	private static Player player;
-	
+
 	private static boolean talking;
-	private static String text;
-	
+	//	private static String text;
+
 	private static Image textImage;
 	private static Graphics textG;
-	
+
 	public static void init(GameContainer gc) throws SlickException {
 		loadMap("room1", "spawn0");
 		int width = gc.getWidth()/Main.SCALE;
@@ -27,13 +27,17 @@ public class World {
 		textImage = new Image(width, height/4);
 		textG = textImage.getGraphics();
 	}
-	
+
 	public static void update(GameContainer gc, int dt) throws SlickException {
 		if(!talking)
 			player.update(gc, dt);
 		if(gc.getInput().isKeyPressed(Input.KEY_SPACE)) {
-			if(talking) talking = false;
-			else chat();
+			if(!talking) chat();
+			else if(Text.done()) {
+				talking = false;
+				Text.reset();
+			}else
+				Text.skip();
 		}
 	}
 	public static void render(GameContainer gc, Graphics g) throws SlickException {
@@ -43,17 +47,19 @@ public class World {
 		int sy = height/2-player.getY();
 		map.render(sx,  sy);
 		player.render(gc, g);
-		if(talking) {
-			textG.setFont(Main.font);
-			textG.setColor(Color.black);
-			textG.fillRect(0, 0, width, height);
-			textG.setColor(Color.white);
-			textG.drawRect(0, 0, width, height);
-			textG.drawString(text, 4, 4);
-			g.drawImage(textImage, 0, 3*height/4);
-		}
+		//		g.setFont((Font) Main.fontLarge);
+		//		Text.render(g);
+		//		if(talking) {
+		//			textG.setFont(Main.font);
+		//			textG.setColor(Color.black);
+		//			textG.fillRect(0, 0, width, height);
+		//			textG.setColor(Color.white);
+		//			textG.drawRect(0, 0, width, height);
+		//			textG.drawString(text, 4, 4);
+		//			g.drawImage(textImage, 0, 3*height/4);
+		//		}
 	}
-	
+
 	private static void chat() {
 		int x = player.getX();
 		int y = player.getY();
@@ -69,7 +75,9 @@ public class World {
 					int ew = map.getObjectWidth(i, j);
 					int eh = map.getObjectHeight(i, j);
 					if(ex + ew > x && x + tw > ex && ey + eh > y && y + th > ey) {
-						text = map.getObjectProperty(i, j, "text", "");
+						Text.say(map.getObjectProperty(i, j, "text", ""),
+								Integer.parseInt(map.getObjectProperty(i, j, "x", "")),
+								Integer.parseInt(map.getObjectProperty(i, j, "y", "")));
 						talking = true;
 						return;
 					}
@@ -77,7 +85,7 @@ public class World {
 			}
 		}
 	}
-	
+
 	public static void loadMap(String strMap, String strSpawn) throws SlickException {
 		map = new TiledMap("res/" + strMap + ".tmx");
 		int objGroupCount = map.getObjectGroupCount();
@@ -91,26 +99,26 @@ public class World {
 			}
 		}
 	}
-	
+
 	public static boolean isPlaceFree(int x, int y) {
 		boolean rightEdge = 1+x/map.getTileWidth() >= map.getWidth();
 		boolean bottomEdge = 1+y/map.getTileHeight() >= map.getHeight();
 		boolean ul = map.getTileId(x/map.getTileWidth(), y/map.getTileHeight(), map.getLayerIndex("Walls")) > 0;
 		boolean ur = rightEdge ? true :
 			map.getTileId(1+x/map.getTileWidth(), y/map.getTileHeight(), map.getLayerIndex("Walls")) > 0;
-		boolean bl = bottomEdge ? true :
-			map.getTileId(x/map.getTileWidth(), 1+y/map.getTileHeight(), map.getLayerIndex("Walls")) > 0;
-		boolean br = rightEdge || bottomEdge ? true :
-			map.getTileId(1+x/map.getTileWidth(), 1+y/map.getTileHeight(), map.getLayerIndex("Walls")) > 0;
-		
-		boolean xAligned = x % map.getTileWidth() == 0;
-		boolean yAligned = y % map.getTileHeight() == 0;
-		
-		if(ul) return false;
-		if(ur && !xAligned) return false;
-		if(bl && !yAligned) return false;
-		if(br && !xAligned && !yAligned) return false;
-		return true;
+			boolean bl = bottomEdge ? true :
+				map.getTileId(x/map.getTileWidth(), 1+y/map.getTileHeight(), map.getLayerIndex("Walls")) > 0;
+				boolean br = rightEdge || bottomEdge ? true :
+					map.getTileId(1+x/map.getTileWidth(), 1+y/map.getTileHeight(), map.getLayerIndex("Walls")) > 0;
+
+					boolean xAligned = x % map.getTileWidth() == 0;
+					boolean yAligned = y % map.getTileHeight() == 0;
+
+					if(ul) return false;
+					if(ur && !xAligned) return false;
+					if(bl && !yAligned) return false;
+					if(br && !xAligned && !yAligned) return false;
+					return true;
 	}
 	public static void checkExits(int x, int y) throws SlickException {
 		int tw = map.getTileWidth();
@@ -132,7 +140,7 @@ public class World {
 			}
 		}
 	}
-	
+
 	public static int getWidth() {
 		return map.getWidth();
 	}
@@ -145,5 +153,5 @@ public class World {
 	public static int getTileHeight() {
 		return map.getTileHeight();
 	}
-	
+
 }
